05748f45a1a77613f2bf6d2b93226cf155062181
harshvalues <business.harshvalues@gmail.com>
2026-09-18 00:24:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  const f = req.url === '/' ? '/index.html' : req.url;
-  const fp = path.join(__dirname, f);
+  const urlPath = req.url.split('?')[0].split('#')[0];
+  const f = urlPath === '/' ? '/index.html' : urlPath;
+  const fp = path.join(__dirname, path.normalize(f).replace(/^(\.\.[\/\\])+/, ''));
   const ext = path.extname(fp);
-  const ct = { 'html': 'text/html', 'js': 'text/javascript', 'css': 'text/css' }[ext.slice(1)] || 'text/plain';
+  const ct = { 'html': 'text/html', 'js': 'text/javascript', 'css': 'text/css', 'xml': 'application/xml', 'txt': 'text/plain' }[ext.slice(1)] || 'text/plain';
   fs.readFile(fp, (err, data) => {
     if (err) { res.writeHead(404); res.end('Not found'); }
     else { res.writeHead(200, { 'Content-Type': ct }); res.end(data); }
